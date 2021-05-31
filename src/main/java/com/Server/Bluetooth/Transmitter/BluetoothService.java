8d015b272360e134de13fe2a9e3da0d100c29c00
Omar Dweikat <omar.dweikat@iscosoft.com>
2021-05-31 16:49:14 +0300

    fixed the bluetooth issue

--- src/main/java/com/Server/Bluetooth/Transmitter/BluetoothService.java
+++ src/main/java/com/Server/Bluetooth/Transmitter/BluetoothService.java
@@ -2,7 +2,6 @@ package com.Server.Bluetooth.Transmitter;
 
 import org.springframework.stereotype.Service;
 
-import javax.microedition.io.Connector;
 import javax.microedition.io.StreamConnection;
 import java.io.IOException;
 import java.io.InputStream;
@@ -10,18 +9,9 @@ import java.io.OutputStream;
 
 @Service
 public class BluetoothService {
-    private final String hc05Url = "btspp://0018E504B1FC:1;authenticate=false;encrypt=false;master=false";
 
 
-    public String processCommand(int command) throws IOException {
-        StreamConnection streamConnection = (StreamConnection) Connector.open(hc05Url);
-        OutputStream os = streamConnection.openOutputStream();
-        InputStream is = streamConnection.openInputStream();
-        byte data = Byte.parseByte(Integer.toString(command));
-        os.write(data); //just send '1' to the device
-        os.close();
-        is.close();
-        streamConnection.close();
-        return "success " + data;
+    public String processCommand(int command, StreamConnection streamConnection, OutputStream os, InputStream is) throws IOException {
+        
     }
 }
--- src/main/java/com/Server/Bluetooth/Transmitter/ServerController.java
+++ src/main/java/com/Server/Bluetooth/Transmitter/ServerController.java
@@ -1,24 +1,60 @@
 package com.Server.Bluetooth.Transmitter;
 
+import javax.microedition.io.Connector;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import javax.microedition.io.StreamConnection;
 
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.io.IOException;
 
 @RestController
 public class ServerController {
 
     private final BluetoothService bluetoothService;
+    private final String hc05Url = "btspp://0018E504B1FC:1;authenticate=false;encrypt=false;master=false";
+    private StreamConnection streamConnection;
+    private OutputStream os;
+    private InputStream is;
 
     public ServerController(BluetoothService bluetoothService) {
         this.bluetoothService = bluetoothService;
     }
 
-    @GetMapping("/send/{cmd}")
-    public String sendBluetoothCommand (@PathVariable("cmd") int command)  {
+    @GetMapping("/start")
+    public String openBluetoothConnection() {
         try {
-            return bluetoothService.processCommand(command);
+            this.streamConnection = (StreamConnection) Connector.open(hc05Url);
+            this.os = streamConnection.openOutputStream();
+            this.is = streamConnection.openInputStream();
+            return "Success";
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return "Fail";
+    }
+
+    @GetMapping("/send/{cmd}")
+    public String sendBluetoothCommand(@PathVariable("cmd") int command) {
+        try {
+            byte data = Byte.parseByte(Integer.toString(command));
+            os.write(data); // just send '1' to the device
+            return "success " + data;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return "Fail";
+    }
+
+    @GetMapping("/stop")
+    public String closeBluetoothConnection() {
+        try {
+            os.close();
+            is.close();
+            streamConnection.close();
+            return "Success";
         } catch (IOException e) {
             e.printStackTrace();
         }
